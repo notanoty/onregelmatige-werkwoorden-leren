4c764624cc7d9c2125bff99712ea5f67364817e9
notanoty <timariefev05@gmail.com>
2026-09-11 15:50:11 +0200

    fix: update url for tracking

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const UMAMI_ORIGIN = "http://146.190.229.9:3005";
+const UMAMI_ORIGIN = "https://umami.notanoty.com";
 
 const nextConfig: NextConfig = {
   async rewrites() {
